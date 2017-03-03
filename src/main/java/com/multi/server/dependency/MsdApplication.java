0cc0cdd62bbd02fb3f27fcb2f3e81b98f52077eb
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2017-03-03 19:09:36 +0530

    Spring Boot Bootstrap code

---
+++ src/main/java/com/multi/server/dependency/MsdApplication.java
@@ -0,0 +1,12 @@
+package com.multi.server.dependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsdApplication.class, args);
+	}
+}
